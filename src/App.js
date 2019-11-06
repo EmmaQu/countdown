09f6a8d952fccb4503dbfbf306c8520f5975e91e
EmmaQu <emmaq481@gmail.com>
2019-11-05 23:00:19 -0800

    Initial countdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {value : 0};
+  }
+  
+  countdown(){
+    this.timerID = setInterval(() => this.countdown(), 1000);
+    this.setState({value: this.state.value -1});
+  }
+  handleChange(e){
+    this.setState({value : e.target.value});
+  }
+  stop(){
+    clearInterval(this.timerID);
+  }
+  render(){
+    return(
+      <div>
+        <h2>Input your count down time here</h2>
+        <input type = "text" onChange = {(e) => this.handleChange(e)} value = {this.state.value}/>
+        <h1>{this.state.value}</h1>
+        <button onClick = {(() =>this.countdown())} >Start</button>
+        <button onClick = {() => this.stop()}>stop</button>
+      </div>
+    );
+  }
 }
-
 export default App;
