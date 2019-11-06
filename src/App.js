fccf0ab9f308c4e9a7bb41b4ddd86727bbf82d4b
EmmaQu <emmaq481@gmail.com>
2019-11-06 00:39:43 -0800

    second

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import logo from './logo.svg';
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {value : 0};
+    this.state = {
+      value : 60,
+     
+    };
   }
-  
+  componentDidMount(){
+    this.timeID = setInterval(() => this.countdown(), 1000);
+  }
   countdown(){
-    this.timerID = setInterval(() => this.countdown(), 1000);
-    this.setState({value: this.state.value -1});
+    if(this.state.value > 0){
+    this.setState({
+      value: this.state.value -1, 
+    });
+  }
   }
   handleChange(e){
     this.setState({value : e.target.value});
   }
-  stop(){
-    clearInterval(this.timerID);
-  }
+ 
   render(){
     return(
       <div>
         <h2>Input your count down time here</h2>
         <input type = "text" onChange = {(e) => this.handleChange(e)} value = {this.state.value}/>
         <h1>{this.state.value}</h1>
-        <button onClick = {(() =>this.countdown())} >Start</button>
-        <button onClick = {() => this.stop()}>stop</button>
+        
       </div>
     );
   }
